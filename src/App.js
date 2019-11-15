fcd96e736acfb68b4345585d6a1807bc9fad18be
ktjhan <tjhan.kavin@gmail.com>
2019-11-15 10:36:01 -0800

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList.js";
+import WelcomePage from "./components/WelcomePage.js";
+import SearchForm from "./components/WelcomePage.js";
+import { Route } from 'react-router-dom';
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/CharacterList" component={CharacterList} />
+      <Route path="/Search" component={SearchForm} />
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,46 @@
 import React from "react";
+import styled from "styled-components";
+// import { Card, CardText, CardTitle, CardBody } from "reactstrap";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Card = styled.div`
+  background-color: dodgerblue;
+  width: 380px;
+  height: 300px;
+  border-radius: 15px;
+  padding: 10px;
+  box-shadow: 0px 1px 4px black;
+  text-align: center;
+  margin: 15px auto;
+`;
+
+const Title = styled.div`
+  font-size: 25px;
+  border-bottom: 1px solid lightgrey;
+  font-weight: 500;
+`;
+
+const CardContent = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const CardImage = styled.img`
+    width: 100px;
+    height: 100px;
+    padding: 5px;
+`;
+
+
+const CharacterCard = props => {
+  return (
+      <Card>
+      <CardImage src={props.image} />
+        <Title>{props.name}</Title>
+        <CardContent>Status:{props.status}</CardContent>
+        <CardContent>Species:{props.species}</CardContent>
+        <CardContent>Type:{props.type}</CardContent>
+      </Card>
+  )
 }
+
+export default CharacterCard;
